35e70958aa685fc10c1c0a8ff78ecc7339ae67f2
Dominik Huppmann <dominik.huppmann@gmail.com>
2021-11-20 13:00:34 +0100

    just some shenanigans

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Message } from '@angular-nest/api-interfaces';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
+  private data = 1;
+
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getData(): Message {
+  getData(): Observable<Message> {
+    console.log('data', this.data);
+    this.data++;
     return this.appService.getData();
   }
 
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Message } from '@angular-nest/api-interfaces';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
+  getData(): Observable<Message> {
+    return of({ message: 'Welcome to api!' }).pipe(
+      map((data) => ({ ...data, message: data.message.toLowerCase() }))
+    );
   }
 }
